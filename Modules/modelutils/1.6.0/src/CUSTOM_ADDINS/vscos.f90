e91a7772d715e45d922efb72cb5e9ba815babf4b
Vincent Vionnet <vincent.vionnet@ec.gc.ca>
2025-12-23 18:52:55 +0000

    Remove all previous version of SVS in MESH code

--- Modules/modelutils/1.6.0/src/CUSTOM_ADDINS/vscos.f90
+++
@@ -1,9 +0,0 @@
-SUBROUTINE vscos(y,x,n)
-!
-INTEGER n
-REAL y(n), x(n)
-!
-y = COS(x)
-!
-RETURN
-END
--- Modules/modelutils/1.6.0/src/CUSTOM_ADDINS/vssin.f90
+++
@@ -1,9 +0,0 @@
-SUBROUTINE vssin(y,x,n)
-!
-INTEGER n
-REAL y(n), x(n)
-!
-y = SIN(x)
-!
-RETURN
-END
