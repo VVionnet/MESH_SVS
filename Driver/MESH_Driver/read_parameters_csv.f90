527204a358555ad3c415bf5a8a1abdb8e2957b30
bbo001 <benjamin.bouchard@ec.gc.ca>
2026-01-06 20:52:30 +0000

    new keys in the mesh_parameters.txt for macropores activation and soil water phase change efficiency

--- Driver/MESH_Driver/read_parameters_csv.f90
+++ Driver/MESH_Driver/read_parameters_csv.f90
@@ -222,6 +222,22 @@ subroutine read_parameters_csv(shd, iun, fname, ierr)
                     call assign_line_args(svs_mesh%vs%lsoil_freezing_svs1, args(2), istat)
                 end if
 
+            case (VN_SVS_LMACROPORES_SVS1)
+                if (.not. svs_mesh%PROCESS_ACTIVE .or. svs_mesh%vs%schmsol=='SVS2' ) then
+                    istat = istat + radix(istat)**pstat%INACTIVE
+                else
+                    p = 1
+                    call assign_line_args(svs_mesh%vs%lmacropores_svs1, args(2), istat)
+                end if
+
+            case (VN_SVS_LPHASE_CHANGE_EFF_SVS1)
+                if (.not. svs_mesh%PROCESS_ACTIVE .or. svs_mesh%vs%schmsol=='SVS2' ) then
+                    istat = istat + radix(istat)**pstat%INACTIVE
+                else
+                    p = 1
+                    call assign_line_args(svs_mesh%vs%lphase_change_eff_svs1, args(2), istat)
+                end if
+
             case (VN_SVS_LWATER_PONDING_SVS)
                 if (.not. svs_mesh%PROCESS_ACTIVE ) then
                     istat = istat + radix(istat)**pstat%INACTIVE
--- LSS_Model/SVS/runsvs_mesh.F90
+++ LSS_Model/SVS/runsvs_mesh.F90
@@ -144,6 +144,8 @@ module runsvs_mesh
     character(len = *), parameter, public :: VN_SVS_NPROFILE_DAY = 'NPROFILE_DAY' ! For svs2 only
     character(len = *), parameter, public :: VN_SVS_LOUT_SVS2_WATBAL = 'LOUT_SVS2_WATBAL ' ! For svs2 only
     character(len = *), parameter, public :: VN_SVS_LSOIL_FREEZING_SVS1 = 'LSOIL_FREEZING_SVS1' ! For svs1 only
+    character(len = *), parameter, public :: VN_SVS_LMACROPORES_SVS1 = 'LMACROPORES_SVS1' ! For svs1 only
+    character(len = *), parameter, public :: VN_SVS_LPHASE_CHANGE_EFF_SVS1 = 'LPHASE_CHANGE_EFF_SVS1' ! For svs1 only
     character(len = *), parameter, public :: VN_SVS_LWATER_PONDING_SVS = 'LWATER_PONDING_SVS' ! For svs1 and svs2
     character(len = *), parameter, public :: VN_SVS_LUNIQUE_PROFILE_SVS2 = 'LUNIQUE_PROFILE_SVS2' ! For svs2 only
     character(len = *), parameter, public :: VN_SVS_LBCHEAT_SVS2 = 'LBCHEAT_SVS2'! For svs2 only
@@ -278,6 +280,8 @@ module runsvs_mesh
         logical :: lout_svs1_watbal = .false.
         integer :: nprofile_day = 4 !
         logical :: lsoil_freezing_svs1 = .false.
+        logical :: lmacropores_svs1 = .false.
+        logical :: lphase_change_eff_svs1 = .false.
         logical :: lwater_ponding_svs = .false.
         logical :: lunique_profile_svs2 = .true.
         character(len = DEFAULT_FIELD_LENGTH) :: lbcheat_svs2 = 'TPERM'
@@ -1019,6 +1023,16 @@ module runsvs_mesh
         ! Activate or not the soil freezing module in SVS1
         if(svs_mesh%vs%schmsol=='SVS') then
                 lsoil_freezing_svs1 = svs_mesh%vs%lsoil_freezing_svs1
+        endif
+
+        ! Activate or not the macropores in SVS1
+        if(svs_mesh%vs%schmsol=='SVS') then
+                lmacropores_svs1 = svs_mesh%vs%lmacropores_svs1
+        endif
+
+        ! Activate or not the phase change efficiency in SVS1
+        if(svs_mesh%vs%schmsol=='SVS') then
+                lphase_change_eff_svs1 = svs_mesh%vs%lphase_change_eff_svs1
         endif
 
         ! Activate or not the water ponding module in SVS1 and SVS2
